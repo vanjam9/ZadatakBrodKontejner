3110c7c6ea7ec839022a59fc9cc8f51e50476c14
Android Development kurs <vanja.males9@gmail.com>
2017-01-20 19:53:22 +0100

    Uradjen zadatak izmena vrednosti

--- src/main/java/zadaci/Zadatak2DodavanjeVrednosti.java
+++ src/main/java/zadaci/Zadatak2DodavanjeVrednosti.java
@@ -1,19 +1,71 @@
 package zadaci;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import model.Brod;
+import model.Kontejner;
+
+
 
 import java.io.IOException;
+
 
 /**
  * Created by androiddevelopment on 20.1.17..
  */
 public class Zadatak2DodavanjeVrednosti {
+
+
+
+        static Dao<Brod,Integer>brodDao;
+        static Dao<Kontejner,Integer>kontejnerDao;
     public static void main(String[] args) {
         ConnectionSource connectionSource = null;
         try {
 
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:brodKontejner.db");
+
+brodDao= DaoManager.createDao(connectionSource,Brod.class);
+kontejnerDao=DaoManager.createDao(connectionSource,Kontejner.class);
+
+
+            TableUtils.clearTable(connectionSource, Brod.class);
+            TableUtils.clearTable(connectionSource, Kontejner.class);
+
+
+            Brod b1 = new Brod("Brod1","Transporter1");
+            brodDao.create(b1);
+
+
+            Brod b2 = new Brod("Brod2","Transporter2");
+            brodDao.create(b2);
+
+            Kontejner k1=new Kontejner("KP1","Namestaj",100);
+            k1.setBrod(b1);
+            kontejnerDao.create(k1);
+
+
+            Kontejner k2=new Kontejner("KP2","Banane",1005);
+            k2.setBrod(b1);
+            kontejnerDao.create(k2);
+
+            Kontejner k3=new Kontejner("KP3","Morski proizvodi",200.7);
+            k3.setBrod(b1);
+            kontejnerDao.create(k3);
+
+            Kontejner k4=new Kontejner("KP4","Mercedes",500.4);
+            k4.setBrod(b2);
+            kontejnerDao.create(k4);
+
+            Kontejner k5=new Kontejner("KP5","Klavir",100);
+            k5.setBrod(b2);
+            kontejnerDao.create(k5);
+
+
+
 
 
         }catch (Exception e) {
--- src/main/java/zadaci/Zadatak3IzmenaVre4dnosti.java
+++ src/main/java/zadaci/Zadatak3IzmenaVre4dnosti.java
@@ -1,20 +1,48 @@
 package zadaci;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import model.Brod;
+import model.Kontejner;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by androiddevelopment on 20.1.17..
  */
 public class Zadatak3IzmenaVre4dnosti {
+
+
+
+
+    static Dao<Brod,Integer> brodDao;
+    static Dao<Kontejner,Integer>kontejnerDao;
     public static void main(String[] args) {
+
+
         ConnectionSource connectionSource = null;
         try {
 
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:brodKontejner.db");
 
+            brodDao= DaoManager.createDao(connectionSource,Brod.class);
+            kontejnerDao=DaoManager.createDao(connectionSource,Kontejner.class);
+
+            List<Kontejner> kontejner = kontejnerDao.queryForAll();
+            for (Kontejner k : kontejner)
+                System.out.println(k);
+
+
+            Kontejner izmena = kontejnerDao.queryForId(k3.getId());
+            izmena.setOpis("Plodovi mora");
+            kontejnerDao.update(izmena);
+
+            List<Kontejner> kontejneri = kontejnerDao.queryForAll();
+            for (Kontejner k1 : kontejneri)
+                System.out.println(k1);
 
         }catch (Exception e) {
             e.printStackTrace();
