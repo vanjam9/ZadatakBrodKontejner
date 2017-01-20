de52f8c0fbad623fc789fbe28eddabdf2f62b772
Android Development kurs <vanja.males9@gmail.com>
2017-01-20 18:36:13 +0100

    Kreirane klase za zadatke fix #3

---
+++ src/main/java/zadaci/Zadatak1KreiranjeTabela.java
@@ -0,0 +1,32 @@
+package zadaci;
+
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+
+import java.io.IOException;
+
+/**
+ * Created by androiddevelopment on 20.1.17..
+ */
+public class Zadatak1KreiranjeTabela {
+    public static void main(String[] args) {
+        ConnectionSource connectionSource = null;
+        try {
+
+            connectionSource = new JdbcConnectionSource("jdbc:sqlite:brodKontejner.db");
+
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (connectionSource != null) {
+                try {
+                    connectionSource.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    }
+
---
+++ src/main/java/zadaci/Zadatak3IzmenaVre4dnosti.java
@@ -0,0 +1,31 @@
+package zadaci;
+
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+
+import java.io.IOException;
+
+/**
+ * Created by androiddevelopment on 20.1.17..
+ */
+public class Zadatak3IzmenaVre4dnosti {
+    public static void main(String[] args) {
+        ConnectionSource connectionSource = null;
+        try {
+
+            connectionSource = new JdbcConnectionSource("jdbc:sqlite:brodKontejner.db");
+
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (connectionSource != null) {
+                try {
+                    connectionSource.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
