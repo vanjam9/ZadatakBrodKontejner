2ba5b90b94424d5544ea37007dc4d2d1b5874e5e
Android Development kurs <vanja.males9@gmail.com>
2017-01-20 19:09:52 +0100

    Uradjen zadatak kreiranje tabela

--- src/main/java/zadaci/Zadatak1KreiranjeTabela.java
+++ src/main/java/zadaci/Zadatak1KreiranjeTabela.java
@@ -2,6 +2,9 @@ package zadaci;
 
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import model.Brod;
+import model.Kontejner;
 
 import java.io.IOException;
 
@@ -14,6 +17,13 @@ public class Zadatak1KreiranjeTabela {
         try {
 
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:brodKontejner.db");
+
+
+            TableUtils.dropTable(connectionSource, Brod.class,true);
+            TableUtils.dropTable(connectionSource, Kontejner.class,true);
+
+            TableUtils.createTable(connectionSource,Brod.class);
+            TableUtils.createTable(connectionSource,Kontejner.class);
 
 
         }catch (Exception e) {
