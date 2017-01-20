bf9dda1464cf1ab6735d2bfd5e56924a8619d1bb
Android Development kurs <vanja.males9@gmail.com>
2017-01-20 20:01:28 +0100

    Uradjen zadtakak izmena vrednosti

--- src/main/java/zadaci/Zadatak1KreiranjeTabela.java
+++ src/main/java/zadaci/Zadatak1KreiranjeTabela.java
@@ -19,8 +19,8 @@ public class Zadatak1KreiranjeTabela {
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:brodKontejner.db");
 
 
-            TableUtils.dropTable(connectionSource, Brod.class,true);
             TableUtils.dropTable(connectionSource, Kontejner.class,true);
+            TableUtils.dropTable(connectionSource, Brod.class,true);
 
             TableUtils.createTable(connectionSource,Brod.class);
             TableUtils.createTable(connectionSource,Kontejner.class);
--- src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
+++ src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
@@ -1,10 +1,57 @@
 package zadaci;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import model.Brod;
+import model.Kontejner;
+
+import java.io.IOException;
+import java.util.List;
+
 /**
  * Created by androiddevelopment on 20.1.17..
  */
 public class Zadatak4BrisanjeVrednosti {
-    public static void main(String[] args) {
 
+
+
+
+    static Dao<Brod,Integer> brodDao;
+    static Dao<Kontejner,Integer>kontejnerDao;
+    public static void main(String[] args) {
+        ConnectionSource connectionSource = null;
+        try {
+
+            connectionSource = new JdbcConnectionSource("jdbc:sqlite:brodKontejner.db");
+            brodDao= DaoManager.createDao(connectionSource,Brod.class);
+            kontejnerDao=DaoManager.createDao(connectionSource,Kontejner.class);
+
+            Kontejner ZaBrisanje = kontejnerDao.queryForId(k1.getId());
+            kontejnerDao.delete(ZaBrisanje);
+
+            List<Kontejner> kontejner = kontejnerDao.queryForAll();
+            for (Kontejner k : kontejner)
+                System.out.println(k);
+
+            Kontejner KP2ZaBrisanje = kontejnerDao.queryForId(k2.getId());
+            kontejnerDao.delete(KP2ZaBrisanje);
+
+            List<Kontejner> kontejner1 = kontejnerDao.queryForAll();
+            for (Kontejner k : kontejner1)
+                System.out.println(k);
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (connectionSource != null) {
+                try {
+                    connectionSource.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
